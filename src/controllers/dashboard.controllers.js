5a694ee74a4d297d9ea1507e09d6b79c4588b4c4
vishal496-beep <you@example.com>
2026-02-27 19:18:41 +0530

    add tweet aggregation to channel stats and photo videos; include author details in responses

--- src/controllers/dashboard.controllers.js
+++ src/controllers/dashboard.controllers.js
@@ -1,22 +1,41 @@
 import mongoose, {isValidObjectId} from "mongoose"
 import {Video} from "../models/video.models.js"
 import {Follower } from "../models/followers.models.js"
-import {Like} from "../models/likes.models.js"
+import {User} from "../models/user.models.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {Photo} from "../models/photo.models.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import { Tweet } from "../models/tweet.models.js"
+
+const AUTHOR_LOOKUP = [
+    {
+        $lookup: {
+            from: "users",
+            localField: "owner",
+            foreignField: "_id",
+            as: "author"
+        }
+    },
+    {
+        $addFields: {
+            author: {$first: "$author"}
+        }
+    }
+]
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
-    const  userId  = req.user?._Id
+    const userId = req.user?._id
     if (!mongoose.isValidObjectId(userId)) {
         throw new ApiError(400, "invalid user id")
     }
     if (!userId) {
         throw new ApiError(400, "Unauthorized request")
     }
-    const {videoStats, photoStats, followCount} = await Promise.all([
+    const user = await User.findById(userId).select("fullname username avatar")
+    if (!user) throw new ApiError(404, "Channel not found")
+    const {videoStats, photoStats,tweetStats, followCount} = await Promise.all([
         //video status
         Video.aggregate([
             {
@@ -64,10 +83,36 @@ const getChannelStats = asyncHandler(async (req, res) => {
                 }
               }
         ]),
+        // tweets likes
+        Tweet.aggregate([
+            {
+                $match: {
+                    owner: new mongoose.Types.ObjectId(userId)
+                }
+            },
+            {
+                $lookup: {
+                    from: "likes",
+                    localField: "_id",
+                    foreignField: "tweet",
+                    as: "allLikes"
+                }
+            },
+            {
+                $group: {
+                    id: null,
+                    count: {$sum: 1},
+                    views: {$sum: "$views"},
+                    totalLikes: {$sum: {$size: "$allLikes"}}
+                }
+            }
+
+        ]),
         Follower.countDocuments({profile: userId})
     ]);
     //merge result 
     const stats = {
+            owner: user,
             followers : followCount || 0,
             video: {
                 total: videoStats[0]?.count || 0,
@@ -77,6 +122,10 @@ const getChannelStats = asyncHandler(async (req, res) => {
             photo: {
                 total: photoStats[0]?.count || 0,
                 likes: photoStats[0]?.totalLikes || 0
+            },
+            tweet: {
+                total: tweetStats[0]?.count || 0,
+                likes: tweetStats[0]?.likes || 0
             }
     }
     return res 
@@ -95,7 +144,7 @@ const getChannelPhotoVideos = asyncHandler(async (req, res) => {
         throw new ApiError(400, "Unauthorized request")
     }
 
-    const [video, photo] = await Promise.all([
+    const [video, photo, tweet] = await Promise.all([
         //1. video aggregation
         Video.aggregate([
             {
@@ -111,6 +160,7 @@ const getChannelPhotoVideos = asyncHandler(async (req, res) => {
                     as: "likes"
                 }
             }, 
+            ...AUTHOR_LOOKUP,
             {
                 $addFields: {
                     likesCount: {$size: "$likes"},
@@ -120,6 +170,9 @@ const getChannelPhotoVideos = asyncHandler(async (req, res) => {
             {
                 $project: {
                     videoFile: 1, 
+                    "author.fullname": 1,
+                    "author.avatar": 1,
+                    "author.username": 1,
                     thumbnail: 1,
                     views: 1,
                     likesCount: 1,
@@ -144,6 +197,7 @@ const getChannelPhotoVideos = asyncHandler(async (req, res) => {
                     as: "likes"
                 }
             },
+             ...AUTHOR_LOOKUP,
             {
                 $addFields: {
                      likesCount: {$size: "$likes"},
@@ -157,13 +211,51 @@ const getChannelPhotoVideos = asyncHandler(async (req, res) => {
                     likesCount: 1,
                     title: 1,
                     type: 1,
-                    createdAt: 1
+                    createdAt: 1,
+                    "author.fullname": 1,
+                    "author.avatar": 1,
+                    "author.username": 1
+
                 }
+            }
+        ]), 
+        // tweet aggregation
+        Tweet.aggregate([
+            {
+                $match: {
+                    owner: new mongoose.Types.ObjectId(userId)
+                }
+            },
+            {
+                 $lookup: {
+                    from : "likes",
+                    localField: "_id",
+                    foreignField: "tweet",
+                    as: "likes"
+                 }
+            },
+             ...AUTHOR_LOOKUP,
+            {
+                $addFields: {
+                    likesCount: {$size: "$likes"},
+                    type: "tweet"
+                }
+            },
+            {
+                 $project: {
+                    content: 1,
+                    likesCount: 1,
+                    type: 1,
+                    "author.fullname": 1,
+                    "author.avatar": 1,
+                    "author.username": 1,
+                    createdAt: 1
+                 }
             }
         ])
     ])
-    const combinedList = [...video, ...photo]
-    combinedList.sort((a, b) => b.createdAt - a.createdAt);
+    const combinedList = [...video, ...photo, ...tweet]
+    combinedList.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
     return res.status(200).json(new ApiResponse(200, combinedList, "videos and photos fetched successfully"))
 })
 
